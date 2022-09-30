35405bc163268a1cd5764bc2307dacf92dd28b2f
Diana Angelova <diana.anguelova@gmail.com>
2022-10-01 00:59:56 +0300

    Update 1-5.sql

--- 04Lab_DataAggregation/1-5.sql
+++ 04Lab_DataAggregation/1-5.sql
@@ -10,10 +10,36 @@ GROUP BY department_id;
 -- 2. Average Salary
 
 SELECT 
-    department_id, round(AVG(salary), 2)
+    department_id, ROUND(AVG(salary), 2)
 FROM
     employees
 GROUP BY department_id;
 
 -- 3. Minimum Salary
 
+SELECT 
+    department_id, MIN(salary) 'Min Salary'
+FROM
+    employees
+GROUP BY department_id
+HAVING MIN(salary) > 800;
+
+-- 4. Appetizers Count
+
+SELECT 
+    COUNT(*)
+FROM
+    products
+WHERE
+    category_id = 2 AND price > 8;
+    
+-- 5. Menu Prices
+
+SELECT 
+    category_id,
+    ROUND(AVG(price), 2) AS 'Average Price',
+    MIN(price) AS 'Cheapest Product',
+    MAX(price) AS 'Most Expensive Product'
+FROM
+    products
+GROUP BY category_id;
