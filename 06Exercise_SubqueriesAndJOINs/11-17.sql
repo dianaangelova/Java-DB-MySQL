69abc4c0b1bf8556ae63199e3dcf8b697f905b89
Diana Angelova <diana.anguelova@gmail.com>
2022-10-09 20:28:26 +0300

    Add files via upload

---
+++ 06Exercise_SubqueriesAndJOINs/11-17.sql
@@ -0,0 +1,54 @@
+-- 11. Min Average Salary
+
+SELECT 
+    AVG(salary) as min_average_salary
+FROM
+    employees
+    group by department_id
+    order by min_average_salary
+    limit 1;
+    
+-- 12. Highest Peaks in Bulgaria
+
+use geography;
+
+SELECT 
+    c.country_code, m.mountain_range, p.peak_name, p.elevation
+FROM
+    peaks p,
+    mountains_countries c,
+    mountains m
+WHERE
+    p.mountain_id = c.mountain_id
+        AND p.mountain_id = m.id
+        AND country_code = 'BG'
+        AND p.elevation > 2835
+ORDER BY p.elevation DESC;
+
+-- 13. Count Mountain Ranges
+
+SELECT 
+    c.country_code, COUNT(m.mountain_range) AS mountain_range
+FROM
+    mountains_countries c,
+    mountains m
+WHERE
+    c.mountain_id = m.id
+        AND country_code IN ('BG' , 'RU', 'US')
+GROUP BY c.country_code
+ORDER BY mountain_range DESC;
+
+-- 14. Countries with Rivers
+
+SELECT 
+    c.country_name, r.river_name
+FROM
+    rivers r,
+    countries_rivers c_rivers,
+    countries c
+WHERE
+    r.id = c_rivers.river_id
+        AND c_rivers.country_code = c.country_code
+        and continent_code = 'AF'
+        order by c.country_name
+        limit 5;
