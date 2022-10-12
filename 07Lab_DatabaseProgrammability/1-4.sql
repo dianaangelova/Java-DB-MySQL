6ec24f2aff4f4a0da6f019832c91d9fe3a237c26
Diana Angelova <diana.anguelova@gmail.com>
2022-10-12 13:55:47 +0300

    Update 1-4.sql

--- 07Lab_DatabaseProgrammability/1-4.sql
+++ 07Lab_DatabaseProgrammability/1-4.sql
@@ -39,3 +39,40 @@ call usp_raise_salaries(NULL);
 
 -- 3. Employees Promotion By ID
 
+DELIMITER $$
+
+create procedure usp_raise_salary_by_id(id INT)
+begin
+declare count_by_id INT;
+start transaction;
+SET count_by_id:= (select count(*) from employees WHERE employee_id = id);
+IF (count_by_id <1) THEN ROLLBACK;
+ELSE UPDATE employees SET     salary = salary + salary * 5 / 100  WHERE     employee_id = id;
+COMMIT;
+END IF;
+end$$
+
+DELIMITER ;
+
+-- 4. Triggered
+
+CREATE TABLE deleted_employees (
+    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    middle_name VARCHAR(50),
+    job_title VARCHAR(50),
+    department_id INT,
+    salary DECIMAL
+);
+
+DELIMITER $$
+
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE
+ON employees
+FOR EACH ROW
+BEGIN
+		INSERT INTO deleted_employees(first_name,last_name,middle_name,job_title,department_id,salary)
+        VALUES (OLD.first_name,OLD.last_name,OLD.middle_name,OLD.job_title,OLD.department_id,OLD.salary);
+END$$
